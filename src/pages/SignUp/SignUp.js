bef5b33f9028b82168383f1a12e4ec5d2c1fcdaa
Chaia Billan <chaia_billan@yahoo.com>
2024-05-15 23:06:22 -0700

    create frontend design of sign up page on mobile and tablet view

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,10 +1,52 @@
 import './SignUp.scss';
 
-function SignUp () {
+function SignUp() {
 
     return (
-        <section className='sign-up'>
-
+        <section className='auth'>
+            <p className='auth__title'>
+                Create an Account
+            </p>
+            <form className='form'>
+                <div className='form__section'>
+                    <label className='form__section--label'>
+                        First Name:
+                    </label>
+                    <input className='form__section--input'>
+                    </input>
+                </div>
+                <div className='form__section'>
+                    <label className='form__section--label'>
+                        Last Name:
+                    </label>
+                    <input className='form__section--input'>
+                    </input>
+                </div>
+                <div className='form__section'>
+                    <label className='form__section--label'>
+                        Create a Username:
+                    </label>
+                    <input className='form__section--input'>
+                    </input>
+                </div>
+                <div className='form__section'>
+                    <label className='form__section--label'>
+                        Create a Password:
+                    </label>
+                    <input className='form__section--input'>
+                    </input>
+                </div>
+                <div className='form__section'>
+                    <label className='form__section--label'>
+                        Confirm Password:
+                    </label>
+                    <input className='form__section--input'>
+                    </input>
+                </div>
+                <button className='form__button normal'>
+                    Create an Account
+                </button>
+            </form>
         </section>
     )
 }
